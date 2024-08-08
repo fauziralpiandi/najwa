bfda685b6518b2ca8a16da2999d316a5295aecee
fauziralpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2024-08-08 22:58:03 +0000

    letsgo

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -59,7 +59,7 @@ function formatDate(date: string) {
   let targetDate = new Date(date).getTime();
   let timeDifference = Math.abs(currentDate - targetDate);
   let daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  
+
   let fullDate = new Date(date).toLocaleString('en-us', {
     month: 'long',
     day: 'numeric',
--- app/db/queries.tsx
+++ app/db/queries.tsx
@@ -3,30 +3,40 @@
 import { sql } from './postgres';
 import { unstable_noStore as noStore } from 'next/cache';
 
-export async function getBlogViews() {
+export async function getBlogViews(): Promise<number> {
   if (!process.env.POSTGRES_URL) {
-    return [];
+    throw new Error('Postgres URL is not defined');
   }
 
   noStore();
-  let views = await sql`
-    SELECT count
-    FROM views
-  `;
+  try {
+    const views = await sql`
+      SELECT count
+      FROM views
+    `;
 
-  return views.reduce((acc, curr) => acc + Number(curr.count), 0);
+    return views.reduce((acc, curr) => acc + Number(curr.count), 0);
+  } catch (error) {
+    console.error('Error fetching blog views:', error);
+    throw new Error('Failed to fetch blog views');
+  }
 }
 
 export async function getViewsCount(): Promise<
   { slug: string; count: number }[]
 > {
   if (!process.env.POSTGRES_URL) {
-    return [];
+    throw new Error('Postgres URL is not defined');
   }
 
   noStore();
-  return sql`
-    SELECT slug, count
-    FROM views
-  `;
+  try {
+    return await sql`
+      SELECT slug, count
+      FROM views
+    `;
+  } catch (error) {
+    console.error('Error fetching views count:', error);
+    throw new Error('Failed to fetch views count');
+  }
 }
